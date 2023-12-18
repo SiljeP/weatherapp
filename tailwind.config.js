7ff20b6dd889f279d732d91d0ad1d9a1c2d01124
SiljeP <u2440913@rts-365.dk>
2023-12-18 09:13:43 +0100

    intial set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'light-primary': '#00000',
+        'light-secondary': '#86878D',
+        'light-tertiary': '#BEBFC4',
+        'light-quaternary': '#D4D5DA',
+        'dark-primary': '#ffffff',
+        'dark-secondary': '#A19EB7',
+        'dark-tertiary': '#6A658A',
+        'dark-quaternary': '#9589B5',
+        'weather-solid-purple':'#48319D',
+        'weather-solid-dark':'#1F1D47',
+        'weather-solid-pink': '#C427FB',
+        'weather-solid-light': '#E0D9FF',
+      }
     },
   },
   plugins: [],
