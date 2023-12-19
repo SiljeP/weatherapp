4f7ebad72b485b8c6d63bee258559f25e1314615
SiljeP <u2440913@rts-365.dk>
2023-12-19 08:52:26 +0100

    House and weather info at start

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,23 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   dfdef
+    <main className="flex flex-col justify-center items-center w-screen bg-no-repeat" 
+    style={{
+       backgroundImage: "url(/images/image.png)", height: "900px", backgroundSize: "cover"
+    }}>
+      <article className=''>
+        <section className='flex flex-col justify-center items-center'>
+          <h1 className="font-main text-4xl font-light text-darkPrimary">Montreal</h1>
+          <p className="font-main text-6xl font-extralight text-darkPrimary">0°C</p>
+          <p className="font-main text-2xl text-darkPrimary">Vejret</p>
+          <p className="font-main text-2xl text-darkPrimary">H: 32°C L:-20°C</p>
+        </section>
+        <Image src="/images/weather-house.png" width={390} height={390} alt="Picture of a cute house" />
+      </article>
+
+
+    </main>
+
+
   )
 }
